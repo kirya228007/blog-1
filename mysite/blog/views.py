8b76e234855350e6b50415edc06eed66fd5d0daf
kirya228007 <kasvitachev@gmail.com>
2024-02-25 12:30:21 +0300

    more evrything

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -12,8 +12,8 @@ def hello(request):
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/posts/list.html', {'posts': posts})
 
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    return render(request, 'blog/posts/detail.html', {'post': post})
