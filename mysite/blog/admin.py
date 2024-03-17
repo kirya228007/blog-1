6e16a2f1126d8c956432487705ec08853fa04ff5
kirya228007 <kasvitachev@gmail.com>
2024-03-17 10:52:15 +0300

    limitles

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Post, Comment
 
 @admin.register(Comment)
@@ -9,10 +10,12 @@ class CommentAdmin(admin.ModelAdmin):
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_display = ['title', 'slug', 'get_image', 'author', 'publish', 'status']
     list_filter = ['status', 'created', 'publish', 'author']
     search_fields = ['title', 'body']
     prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ['author']
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
+    def get_image(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" weight="50" height="50">') if obj.image else ''
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.conf import settings
 from . import views
 
 app_name = 'blog'
@@ -7,5 +7,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
-    path('<int:post_id>/comment/', views.post_comment, name='post_comment')
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('', views.upload_display_video, name='upload_display_video')
 ]
