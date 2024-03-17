1264ca6f1ef1ccf82997e4d07432ea7076737328
kirya228007 <kasvitachev@gmail.com>
2024-03-17 10:53:11 +0300

    limitles 2

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
-    path('', views.upload_display_video, name='upload_display_video')
 ]
